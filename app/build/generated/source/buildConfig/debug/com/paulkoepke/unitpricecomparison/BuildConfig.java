7d8df0a24b1f822b76553b92be681fba78736aa0
Paul Koepke <paul.koepke@gmail.com>
2020-10-08 18:47:48 -0500

    Added minify and shrink settings to gradle build script. Removing support for Android versions before 17 as they seem to break the buttons at the top of the screen.

--- app/build/generated/source/buildConfig/debug/com/paulkoepke/unitpricecomparison/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/paulkoepke/unitpricecomparison/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.paulkoepke.unitpricecomparison";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 201;
-  public static final String VERSION_NAME = "2.0.1";
+  public static final int VERSION_CODE = 204;
+  public static final String VERSION_NAME = "2.0.4";
 }
