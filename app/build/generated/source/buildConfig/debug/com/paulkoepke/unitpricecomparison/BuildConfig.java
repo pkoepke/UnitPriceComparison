86e32a4ee86d64a41433e8521fefd6acdfe828c1
Paul Koepke <paul.koepke@gmail.com>
2020-11-24 16:41:10 -0600

    Added adaptive icons and got them working. Didn't change the version or publish it, just playing with adaptive icons for now.

--- app/build/generated/source/buildConfig/debug/com/paulkoepke/unitpricecomparison/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/paulkoepke/unitpricecomparison/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.paulkoepke.unitpricecomparison";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 204;
-  public static final String VERSION_NAME = "2.0.4";
+  public static final int VERSION_CODE = 207;
+  public static final String VERSION_NAME = "2.0.7";
 }
